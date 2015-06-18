3066cd6eaaae58eb65e6723dbae93f93444898ea
Saugat Acharya <mesaugat@gmail.com>
2015-06-18 22:03:27 +0545

    Simple bullet reflection

--- js/Game.js
+++ js/Game.js
@@ -8,6 +8,9 @@ var playState = {
     },
 
     create: function () {
+        this.circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 75 * 2);
+        this.timer = game.add.text(16, 16, '00:00', {fontSize: '32px', fill: '#000'});
+
         game.stage.backgroundColor = '#3498db';
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -17,50 +20,68 @@ var playState = {
         this.circle.scale.y = 0.123;
         this.circle.anchor.setTo(0.5, 0.5);
 
-        this.shield = game.add.sprite(game.world.centerX + 50, game.world.centerY, 'shield');
+        this.shield = game.add.sprite(game.world.centerX + 75, game.world.centerY, 'shield');
         game.physics.arcade.enable(this.shield);
 
         this.shield.angle = 0;
         this.shield.radius = 75;
-        this.shield.scale.x = 0.5;
-        this.shield.scale.y = 0.5;
-        this.shield.anchor.setTo(0.5, 0.5);
+        this.shield.scale.x = 0.6;
+        this.shield.scale.y = 0.6;
+        this.shield.anchor.set(0.2);
+
+        this.rotation = 0;
+        this.rotationSpeed = 0.18;
 
         var circle = this.circlePoints(180);
         this.bullet = game.add.sprite(circle.x, circle.y, 'bullet');
         game.physics.arcade.enable(this.bullet);
         this.bullet.rotation = game.physics.arcade.angleBetween(this.bullet, this.circle);
-        game.physics.arcade.moveToObject(this.bullet, this.circle, 250);
+        game.physics.arcade.moveToObject(this.bullet, this.circle, 400);
 
         this.cursor = game.input.keyboard.createCursorKeys();
     },
 
     update: function () {
         this.moveShield();
-        var collide = game.physics.arcade.collide(this.bullet, this.shield);
+        this.updateTimer();
+
+        game.physics.arcade.overlap(this.shield, this.bullet, this.reflectBack, null, this);
+    },
+
+    reflectBack: function (shield, bullet) {
+        this.bullet.body.velocity.x = -2 * bullet.body.velocity.x;
+        this.bullet.body.velocity.y = -2 * bullet.body.velocity.y;
+    },
+
+    updateTimer: function () {
+        var seconds = Math.floor(game.time.now / 1000) % 60;
+        var milliseconds = Math.floor(game.time.now) % 60;
+
+        if (milliseconds < 10) milliseconds = '0' + milliseconds;
+        if (seconds < 10) seconds = '0' + seconds;
+        this.timer.setText(seconds + ':' + milliseconds);
     },
 
     moveShield: function () {
-        // If the left arrow key is pressed
         if (this.cursor.left.isDown) {
-            this.shield.angle -= 0.25;
-            this.shield.x = game.world.centerX + this.shield.radius * Math.cos(this.shield.angle);
-            this.shield.y = game.world.centerY + this.shield.radius * Math.sin(this.shield.angle);
-            this.shield.rotation = this.game.physics.arcade.angleBetween(this.shield, this.circle);
+            this.shield.rotation = this.rotation + Math.PI / 2;
+
+            this.shield.x = game.world.centerX + Math.cos(this.rotation) * ( this.shield.radius + this.shield.height / 2 );
+            this.shield.y = game.world.centerY + Math.sin(this.rotation) * ( this.shield.radius + this.shield.height / 2 );
+
+            this.rotation -= this.rotationSpeed;
         }
-        // If the right arrow key is pressed
         else if (this.cursor.right.isDown) {
-            this.shield.angle += 0.25;
-            this.shield.x = game.world.centerX + this.shield.radius * Math.cos(this.shield.angle);
-            this.shield.y = game.world.centerY + this.shield.radius * Math.sin(this.shield.angle);
-            this.shield.rotation = this.game.physics.arcade.angleBetween(this.shield, this.circle);
-        }
+            this.shield.rotation = this.rotation + Math.PI / 2;
 
-        // If neither the right or left arrow key is pressed
+            this.shield.x = game.world.centerX + Math.cos(this.rotation) * ( this.shield.radius + this.shield.height / 2 );
+            this.shield.y = game.world.centerY + Math.sin(this.rotation) * ( this.shield.radius + this.shield.height / 2 );
+
+            this.rotation += this.rotationSpeed;
+        }
         else {
-            // Stop the player
+            this.shield.rotation = this.rotation + Math.PI / 2;
         }
-
     },
 
     bullet: function () {
