23bc9d6fd65600c1d226b56afc1a7f54da51f404
Saugat Acharya <mesaugat@gmail.com>
2015-06-20 22:31:28 +0545

    Add blue bullet and check collision with enemy.

--- js/Game.js
+++ js/Game.js
@@ -1,43 +1,47 @@
-var game = new Phaser.Game(800, 400, Phaser.CANVAS, 'de-ghumake-game');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'de-ghumake-game');
 
 var playState = {
     preload: function () {
         game.load.image('circle', 'assets/images/circle.png');
         game.load.image('shield', 'assets/images/reflector.png');
-        game.load.image('bullet', 'assets/images/bullet.png');
+        game.load.image('redBullet', 'assets/images/red-bullet.png');
+        game.load.image('blueBullet', 'assets/images/blue-bullet.png')
         game.load.image('enemy', 'assets/images/enemy.png');
     },
 
     create: function () {
-        this.circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 75 * 2);
         this.timer = game.add.text(16, 16, '00:00', {fontSize: '32px', fill: '#000'});
 
         game.stage.backgroundColor = '#3498db';
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        game.scale.refresh();
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        this.circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 75 * 2);
         this.circle = game.add.sprite(game.world.centerX, game.world.centerY, 'circle');
         this.circle.scale.x = 0.00123;
         this.circle.scale.y = 0.00123;
         this.circle.anchor.setTo(0.5, 0.5);
 
         this.enemies = game.add.group();
-        this.bullets = game.add.group();
+        this.redBullets = game.add.group();
+        this.blueBullets = game.add.group();
 
         this.shield = game.add.sprite(game.world.centerX + 75, game.world.centerY, 'shield');
         game.physics.arcade.enable(this.shield);
 
         this.shield.angle = 0;
         this.shield.radius = 75;
-        this.shield.scale.x = 0.6;
-        this.shield.scale.y = 0.6;
+        this.shield.scale.x = 0.8;
+        this.shield.scale.y = 0.8;
         this.shield.anchor.set(0.5, 0.5);
 
         this.rotation = 0;
         this.rotationSpeed = 0.18;
 
         this.generateEnemyPositions();
-        //  Once the event has been called 10 times it will never be called again.
 
         game.time.events.repeat(Phaser.Timer.SECOND * 1, 25, this.spawnEnemy, this);
 
@@ -48,25 +52,30 @@ var playState = {
         this.moveShield();
         this.updateTimer();
 
-        game.physics.arcade.overlap(this.shield, this.bullets, this.reflectBack, null, this);
-        game.physics.arcade.collide(this.bullets, this.enemies, this.helloYamraj, null, this);
+        game.physics.arcade.overlap(this.shield, this.redBullets, this.reflectBack, null, this);
+        game.physics.arcade.collide(this.blueBullets, this.enemies, this.deathHandler, null, this);
 
         if (this.cursor.up.isDown) {
             this.spawnEnemy();
         }
     },
 
-    helloYamraj: function (bullet, enemy) {
-        bullet.kill();
+    deathHandler: function (blueBullet, enemy) {
+        blueBullet.kill();
         enemy.kill();
     },
 
     generateEnemyPositions: function () {
         var enemyHeight = 20;
         this.enemyPositions = [
-            [8 * enemyHeight, 3 * enemyHeight], [game.world.centerX, enemyHeight], [game.world.width - 2 * enemyHeight, 2 * enemyHeight],
-            [enemyHeight, game.world.centerY], [game.world.width - 2 * enemyHeight, game.world.centerY],
-            [enemyHeight, game.world.height - 2 * enemyHeight], [game.world.centerX, game.world.height - enemyHeight], [game.world.width - 2 * enemyHeight, game.world.height - enemyHeight]
+            [8 * enemyHeight, 3 * enemyHeight],
+            [game.world.centerX, enemyHeight],
+            [game.world.width - 2 * enemyHeight, 2 * enemyHeight],
+            [enemyHeight, game.world.centerY],
+            [game.world.width - 2 * enemyHeight, game.world.centerY],
+            [enemyHeight, game.world.height - 2 * enemyHeight],
+            [game.world.centerX, game.world.height - enemyHeight],
+            [game.world.width - 2 * enemyHeight, game.world.height - enemyHeight]
         ]
     },
 
@@ -78,23 +87,32 @@ var playState = {
         enemy.scale.y = 4;
         enemy.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(enemy);
+
         this.enemies.add(enemy);
         this.fireBullet(enemy);
     },
 
     fireBullet: function (enemy) {
-        var enemy = enemy;
-        var bullet = game.add.sprite(enemy.x, enemy.y, 'bullet');
-        bullet.anchor.setTo(0.5, 0.5);
-        game.physics.arcade.enable(bullet);
-        bullet.rotation = game.physics.arcade.angleBetween(bullet, this.circle);
-        game.physics.arcade.moveToObject(bullet, this.circle, 400);
-        this.bullets.add(bullet);
+        var redBullet = game.add.sprite(enemy.x, enemy.y, 'redBullet');
+        redBullet.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(redBullet);
+        redBullet.rotation = game.physics.arcade.angleBetween(redBullet, this.circle);
+        game.physics.arcade.moveToObject(redBullet, this.circle, 500);
+
+        this.redBullets.add(redBullet);
     },
 
-    reflectBack: function (shield, bullet) {
-        bullet.body.velocity.x = -2 * bullet.body.velocity.x;
-        bullet.body.velocity.y = -2 * bullet.body.velocity.y;
+    reflectBack: function (shield, redBullet) {
+        redBullet.kill();
+        var blueBullet = game.add.sprite(redBullet.position.x, redBullet.position.y, 'blueBullet');
+        blueBullet.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(blueBullet);
+
+        blueBullet.rotation = redBullet.rotation;
+        blueBullet.body.velocity.x = -2 * redBullet.body.velocity.x;
+        blueBullet.body.velocity.y = -2 * redBullet.body.velocity.y;
+
+        this.blueBullets.add(blueBullet);
     },
 
     updateTimer: function () {
@@ -127,7 +145,6 @@ var playState = {
             this.shield.rotation = this.rotation + Math.PI / 2;
         }
     }
-
 };
 
 game.state.add('playState', playState);
