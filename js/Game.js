b95c76c4384761ee707b45b68b24431e323659a0
prakash <prakashpoudel@lftechnology.com>
2015-06-26 09:21:32 +0545

    enemy appears from enemy circle points

--- js/Game.js
+++ js/Game.js
@@ -1,8 +1,8 @@
-var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'de-ghumake-game');
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'de-ghumake-game');
 
 var playState = {
     preload: function () {
-        game.load.image('circle', 'assets/images/circle.png');
+        game.load.image('player', 'assets/images/circle.png');
         game.load.image('shield', 'assets/images/reflector.png');
         game.load.image('redBullet', 'assets/images/red-bullet.png');
         game.load.image('blueBullet', 'assets/images/blue-bullet.png')
@@ -10,12 +10,14 @@ var playState = {
     },
 
     create: function () {
+
         this.timer = game.add.text(16, 16, '00:00', {fontSize: '32px', fill: '#000'});
 
         this.score = 0;
         this.life = 10;
-        this.spawnDelay = 1600;
-        this.spawnTimer=game.time.time;
+
+        this.spawnDelay = 1600; 
+        this.spawnTimer = game.time.time;
 
         game.stage.backgroundColor = '#3498db';
         game.scale.pageAlignHorizontally = true;
@@ -24,17 +26,16 @@ var playState = {
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        this.circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 75 * 2);
-        this.circle = game.add.sprite(game.world.centerX, game.world.centerY, 'circle');
-        this.circle.scale.x = 0.00123;
-        this.circle.scale.y = 0.00123;
-        this.circle.anchor.setTo(0.5, 0.5);
+        this.enemyRadius = game.world.height/2.7;
+
+        this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        this.player.scale.x = 0.00123;
+        this.player.scale.y = 0.00123;
+        this.player.anchor.setTo(0.5, 0.5);
 
         this.enemies = game.add.group();
         this.redBullets = game.add.group();
-        this.redBullets.enableBody = true;
         this.blueBullets = game.add.group();
-        this.blueBullets.enableBody = true;
 
         this.shield = game.add.sprite(game.world.centerX + 75, game.world.centerY, 'shield');
         game.physics.arcade.enable(this.shield);
@@ -47,10 +48,6 @@ var playState = {
 
         this.rotation = 0;
         this.rotationSpeed = 0.18;
-
-        this.generateEnemyPositions();
-
-        // game.time.events.repeat(Phaser.Timer.SECOND * 1, 25, this.spawnEnemy, this);
 
         this.cursor = game.input.keyboard.createCursorKeys();
     },
@@ -84,27 +81,12 @@ var playState = {
         enemy.kill();
     },
 
-    generateEnemyPositions: function () {
-        var enemyHeight = 20;
-        this.enemyPositions = [
-            [8 * enemyHeight, 3 * enemyHeight],
-            [game.world.centerX, enemyHeight],
-            [game.world.width - 2 * enemyHeight, 2 * enemyHeight],
-            [enemyHeight, game.world.centerY],
-            [game.world.width - 2 * enemyHeight, game.world.centerY],
-            [enemyHeight, game.world.height - 2 * enemyHeight],
-            [game.world.centerX, game.world.height - enemyHeight],
-            [game.world.width - 2 * enemyHeight, game.world.height - enemyHeight]
-        ]
-    },
-
     spawnEnemy: function () {
-
-        var enemyPosition = this.enemyPositions[game.rnd.integerInRange(0, 7)];
-        var enemy = game.add.sprite(enemyPosition[0], enemyPosition[1], 'enemy');
-        enemy.rotation = this.game.physics.arcade.angleBetween(enemy, this.circle);
-        enemy.scale.x = 4;
-        enemy.scale.y = 4;
+        var enemyPosition = this.circlePoints(this.enemyRadius);
+        var enemy = game.add.sprite(enemyPosition.x, enemyPosition.y, 'enemy');
+        enemy.rotation = this.game.physics.arcade.angleBetween(enemy, this.player);
+        enemy.scale.x = 3;
+        enemy.scale.y = 3;
         enemy.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(enemy);
 
@@ -121,16 +103,16 @@ var playState = {
         var redBullet = game.add.sprite(enemy.x, enemy.y, 'redBullet');
         redBullet.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(redBullet);
-        redBullet.rotation = game.physics.arcade.angleBetween(redBullet, this.circle);
-        game.physics.arcade.moveToObject(redBullet, this.circle, 500);
+        redBullet.rotation = game.physics.arcade.angleBetween(redBullet, this.player);
+        game.physics.arcade.moveToObject(redBullet, this.player, 500);
         
-        redBullet.events.onOutOfBounds.add( this.goodbye );
+        redBullet.events.onOutOfBounds.add( this.bulletOut );
 
         this.redBullets.add(redBullet);
     },
 
-// function that gets called when bullet goes out of sight
-    goodbye: function(redBullet){
+    // function that gets called when bullet goes out of sight
+    bulletOut: function(redBullet){
         console.log('ef');
         redBullet.kill();
         this.clearEnemies();
@@ -178,6 +160,21 @@ var playState = {
         else {
             this.shield.rotation = this.rotation + Math.PI / 2;
         }
+    },
+
+    circlePoints: function (radius) {
+        var angle = game.rnd.integerInRange(0, 360);
+        var x = radius * Math.cos(this.toRadians(angle)) + game.world.centerX;
+        var y = radius * Math.sin(this.toRadians(angle)) + game.world.centerY;
+
+        return {
+            x: x,
+            y: y
+        };
+    },
+
+    toRadians: function (angle) {
+        return angle * (Math.PI / 180);
     }
 };
 
