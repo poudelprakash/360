c832cd59bdb5802c6e70f8a2077b33d35ef9c20b
Saugat Acharya <mesaugat@gmail.com>
2015-06-28 19:08:07 +0545

    Fix timer and add game over state.

--- js/gamestart.js
+++ js/gamestart.js
@@ -5,14 +5,16 @@ var gameStart = function (game) {
 gameStart.prototype = {
     create: function () {
         this.music = this.add.audio('music');
+        this.music.loop = true;
         this.music.play();
 
         var playButton = this.add.text(this.world.centerX, this.world.centerY, 'Play', {
             font: "40px roboto_slabregular",
-            fill: '#fff'
+            fill: '#edc53f'
         });
         playButton.anchor.setTo(0.5);
         playButton.inputEnabled = true;
+        playButton.input.useHandCursor = true;
         playButton.events.onInputDown.add(function () {
             this.state.start('Play');
         }, this);
